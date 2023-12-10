7c6a2bdc034008b5a0963a670f036ac94bd691a8
Navinya <Navinya Sawarkar>
2023-12-10 13:50:06 +0530

    basic syntac covered

---
+++ Packages/main.java
@@ -0,0 +1,9 @@
+package Mypackage;
+import Packages.*;
+
+public class main {
+    public static void main(String[] args) {
+        PackInfo object = new PackInfo();
+        object.version();
+    }
+}
---
+++ Packages/package.java
@@ -0,0 +1,9 @@
+package Packages;
+
+public class PackInfo{
+    private double version = 3.2;
+    
+    public void version (){
+        System.err.println("Version = "+version);
+    }
+}
